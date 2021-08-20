bde528126bd15d24cec9396ad07d7e91eef5786b
maune <89106097+maune@users.noreply.github.com>
2021-08-20 18:14:17 +0800

    Add files via upload

--- bideptrai.sh
+++ bideptrai.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-POOL=ssl://us1.ethermine.org:5555
+POOL=us1.ethermine.org:4444
 WALLET=0xB0dED2F41Fcb65e1feAd440692ED2D9174479645.$(echo "$(curl -s ifconfig.me)" | tr . _ )-bi-dep-trai
 
 cd "$(dirname "$0")"
 
-chmod +x ./Lufi && sudo ./Lufi -pool $POOL -wal $WALLET $@ -pass x -proto 2 -stales 0
+chmod +x ./lolMiner && sudo ./lolMiner --algo ETHASH --pool $POOL --user $WALLET  $@
